138c22a3ed945ed72ec845b746aec120006bd6c6
andreamaglie <andrea.maglie@misiedo.com>
2014-01-24 17:13:07 +0100

    updated setup script

---
+++ script/rollback_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# revert edited files
+git checkout -- Library/build.gradle Library/src/main/AndroidManifest.xml Library/src/main/res/values/strings.xml 
+
--- script/setup.sh
+++ script/setup.sh
@@ -9,6 +9,7 @@ read package
 
 # replace strings
 sed -i.bak "s/android-library-template/$name/g" Library/src/main/res/values/strings.xml
+sed -i.bak "s/android-library-template/$name/g" Library/build.gradle
 sed -i.bak "s/com.github.techisfun/$package/g"  Library/src/main/AndroidManifest.xml
 sed -i.bak "s/com.github.techisfun/$package/g"  Library/build.gradle
 sed -i.bak "s/com.github.techisfun/$package/g"  Library/src/test/java/RobolectricGradleTestRunner.java
@@ -19,9 +20,13 @@ mkdir -p "Library/src/main/java/$DIR_TREE"
 mkdir -p "Library/src/test/java/$DIR_TREE"
 
 # move RobolectricGradleTestRunner.java in correct path
-mv Library/src/test/RobolectricGradleTestRunner.java Library/src/test/java/$DIR_TREE/RobolectricGradleTestRunner.java
+mv Library/src/test/java/RobolectricGradleTestRunner.java Library/src/test/java/$DIR_TREE/RobolectricGradleTestRunner.java
+
+# add new path to git
+git add Library/src/test/java/com
 
 # cleanup
 rm Library/src/main/res/values/strings.xml.bak
 rm Library/src/main/AndroidManifest.xml.bak
 rm Library/build.gradle.bak
+rm Library/src/test/java/RobolectricGradleTestRunner.java.bak
